0e84be910a7f25c9291090adf6ca5c4dade29faa
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2017-06-04 17:59:40 +0800

    add service to recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
-import { Recipe } from '../recipe.model'
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,15 +10,13 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'test',
-    'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Another test Recipe', 'test',
-    'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg')
-  ];
-  constructor() { }
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) {
+  }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,17 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'test',
+    'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Another test Recipe', 'test',
+    'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg')
+  ];
+
+  getRecipes() {
+    //why slice? so we only get copy of recipes
+    // - and not change the recipes in this file
+    //(arr is obj so pass by reference = need to make copy to prevent pass by reference )
+    return this.recipes.slice();
+  }
+}
